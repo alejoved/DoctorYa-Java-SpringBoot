d2981fd46ee4b5978a2eff26b345b19a204b22a7
Jorge Alejandro Aguirre Gutierrez <joaaguir@bancolombia.com.co>
2025-05-15 16:30:22 -0500

    Add getByIdentification

--- src/main/java/com/project/doctorya/patient/dto/PatientResponseDTO.java
+++ src/main/java/com/project/doctorya/patient/dto/PatientResponseDTO.java
@@ -1,7 +1,5 @@
 package com.project.doctorya.patient.dto;
 
-import java.util.UUID;
-
 import com.project.doctorya.auth.dto.AuthResponseDTO;
 
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -9,8 +7,6 @@ import lombok.Data;
 
 @Data
 public class PatientResponseDTO {
-    @Schema(description = "Appointment ID")
-    private UUID  id;
     @Schema(description = "Patient full name")
     private String name;
     @Schema(description = "Name of the heatlh insurance")
--- src/main/java/com/project/doctorya/physician/dto/PhysicianResponseDTO.java
+++ src/main/java/com/project/doctorya/physician/dto/PhysicianResponseDTO.java
@@ -1,7 +1,5 @@
 package com.project.doctorya.physician.dto;
 
-import java.util.UUID;
-
 import com.project.doctorya.auth.dto.AuthResponseDTO;
 
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -9,8 +7,6 @@ import lombok.Data;
 
 @Data
 public class PhysicianResponseDTO {
-    @Schema(description = "Appointment ID")
-    private UUID  id;
     @Schema(description = "Physician full name")
     private String name;
     @Schema(description = "General medical code")
